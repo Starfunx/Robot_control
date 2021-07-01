2eb1730069b4c26eb135f1e670d4cafb2f7ebc37
S6ril <cyril.tessier@gmail.com>
2021-07-01 10:46:49 +0200

    intergation des modules dans le robot. Déplacement de 0 à 1000 mm puis vers 0 (suivant l'axe x)

--- src/main_task.py
+++ src/main_task.py
@@ -1,14 +1,36 @@
 from external.API_interface.TLF_API.component.Class_Pose2D import Pose2D
-from module.servo import servo_init, servo_move_max, servo_move_min
+from gpiozero.pins.rpigpio import RPiGPIOFactory
+from gpiozero import Servo, Device
+from module.servo_cote import Servo_cote
 
 
 from robot_package.RobotControl import RobotControl
 from task_manager.task_manager import Task
 import time
 
-def deplacer1():
+
+
+#########################################
+# Creation de la stratégie
+#########################################
+
+def deplacer_1000():
     global robotcontrol
-    goal = [Pose2D(1000, 0, 0)]
+    goal = Pose2D(1000, 0, 0)
+    robotcontrol.set_goal(goal)
+
+    robotcontrol.robot.enable_motors()
+
+    while not(robotcontrol.goal_reached()):
+        robotcontrol.update_speed()
+    
+    robotcontrol.robot.set_speed(0, 0)
+    robotcontrol.robot.disable_motors()
+
+
+def deplacer_0():
+    global robotcontrol
+    goal = Pose2D(0, 0, 0)
     robotcontrol.set_goal(goal)
 
     robotcontrol.robot.enable_motors()
@@ -26,52 +48,101 @@ def stop_robot():
     robotcontrol.robot.disable_motors()
 
 def deploye_servo():
-    global SERVO
-    servo_move_max(SERVO)
-    time.sleep(2)
+    global servo_cote
+    servo_cote.move_max()
+    time.sleep(0.1)
 
 
 def retract_servo():
-    global SERVO
-    servo_move_min(SERVO)
+    global servo_cote
+    servo_cote.move_min()
+    time.sleep(0.1)
+
+def wait():
+    time.sleep(0.3)
+
+def wait2():
     time.sleep(2)
 
 
+# Creation des tasks
+task_deplacer_1000 = Task(deplacer_1000)
+task_deplacer_0 = Task(deplacer_0)
+
+task_deploye_servo = Task(deploye_servo)
+task_retract_servo = Task(retract_servo)
+task_wait = Task(wait)
+task_wait_deplacement = Task(wait2)
+
+############################################
+# Création du robot
+############################################
+
+# Carte moteur
 nom_fichier = './config/robot_config.yaml'
 robot_port = "/dev/ttyACM0"
 robot_baurate = 115200
 
+# Carte obstacle
 carte_obstacle_port = ""
 carte_obstacle_bauderate = 115200
 
 robotcontrol = RobotControl(nom_fichier, robot_port, robot_baurate, carte_obstacle_port, carte_obstacle_bauderate)
-SERVO = servo_init()
 
-task_deplacer1 = Task(deplacer1)
-task_servo1 = Task(deploye_servo)
-task_servo2 = Task(retract_servo)
 
-Tmax = 20.0 #secondes
+###########################################
+# Creation des modules
+###########################################
+
+# Module servo sur le coté
+Device.pin_factory = RPiGPIOFactory()
+servo_gauche = Servo(17)
+servo_droite = Servo(27)
+
+servo_cote = Servo_cote(servo_droite, servo_gauche)
+
+
+#########################################
+# Main programme
+##########################################
+
+
+
+Tmax = 15.0 #secondes
 # start
 t0 = time.time()
 t = t0
 # Main loop
 while t < t0+Tmax:
 
-    task_deplacer1.start()
+    task_deplacer_1000.start()
+    task_deploye_servo.start()
+
+    if task_deplacer_1000.done:
+        task_wait_deplacement.start()
+    
+    if task_wait_deplacement.done:
+        task_deplacer_0.start()
     
 
-    if task_deplacer1.done:
-        task_servo1.start()
+    if task_deploye_servo.done:
+        task_wait.start()
     
-    if task_servo1.done:
-        task_servo2.start()
+    if task_wait.done:
+        task_retract_servo.start()
 
     t = time.time()
 
-task_deplacer1.stop()
-task_servo1.stop()
-task_servo2.stop()
+# End loop
+
+
+
+task_deplacer_1000.stop()
+task_deplacer_0.stop()
+task_deploye_servo.stop()
+task_retract_servo.stop()
+task_wait.stop()
+task_wait_deplacement.stop()
 
 stop_robot() 
 
